d314c86f18b15ec377ec86ae551d946792ffb2c5
SerkanUgiSoft <serkan.ulusoy@ugisoft.com.tr>
2023-09-04 14:21:30 +0300

    Add project files.

---
+++ JWTAuth.WebApi/Extensions/ModulesExtension.cs
@@ -0,0 +1,42 @@
+﻿namespace JWTAuth.WebApi.Extensions
+{
+    public interface IModule
+    {
+        IServiceCollection RegisterModules(IServiceCollection builder);
+        IEndpointRouteBuilder MapEndpoints(IEndpointRouteBuilder endpoints);
+    }
+
+    public static class ModuleExtensions
+    {
+        // this could also be added into the DI container
+        private static readonly List<IModule> RegisteredModules = new();
+
+        public static IServiceCollection RegisterModules(this IServiceCollection services)
+        {
+            var modules = DiscoverModules();
+            foreach (var module in modules)
+            {
+                module.RegisterModules(services);
+                RegisteredModules.Add(module);
+            }
+
+            return services;
+        }
+
+        public static WebApplication MapEndpoints(this WebApplication app)
+        {
+            foreach (var module in RegisteredModules) module.MapEndpoints(app);
+
+            return app;
+        }
+
+        private static IEnumerable<IModule> DiscoverModules()
+        {
+            return typeof(IModule).Assembly
+                .GetTypes()
+                .Where(p => p.IsClass && p.IsAssignableTo(typeof(IModule)))
+                .Select(Activator.CreateInstance)
+                .Cast<IModule>();
+        }
+    }
+}
---
+++ JWTAuth.WebApi/Modules/Jwt/JwtModule.cs
@@ -0,0 +1,25 @@
+﻿using JWTAuth.WebApi.Extensions;
+using JWTAuth.WebApi.Models;
+using JWTAuth.WebApi.Modules.Jwt.Endpoints;
+
+namespace JWTAuth.WebApi.Modules.Jwt
+{
+    public class JwtModule :IModule
+    {
+        public IServiceCollection RegisterModules(IServiceCollection services)
+        {
+            services.AddTransient<JwtEndpoints>();
+            return services;
+        }
+        public IEndpointRouteBuilder MapEndpoints(IEndpointRouteBuilder endpoints)
+        {
+            var rootConfigurations = endpoints.MapGroup("").WithTags("Jwt");
+            rootConfigurations.MapPost("token/", async (UserInfo model,JwtEndpoints scanEndpoint) =>
+                await scanEndpoint.Post(model));
+
+           
+
+            return endpoints;
+        }
+    }
+}
